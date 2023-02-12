806925980a6eab90bff77725860c826d9b26aa95
Sergii Oleshchenko <nightzone@ukr.net>
2023-02-12 11:24:53 +0200

    v0.1 - dev

---
+++ README.md
@@ -0,0 +1,18 @@
+Redfish Walker
+==============
+Script walks through redfish oids and collects data
+
+### Features:
+
+### Usage:
+
+### Requirements:
+* Microsoft .NET 4.5 installed
+* Tested on Windows 10 and PowerShell 5.1
+* Tested on Windows Server 2022 and PowerShell 5.1
+* Tested on Windows Server 2012 R2 and PowerShell 4.0
+
+### Fixes:
+
+### Author:
+Sergii Oleshchenko<br/>
---
+++ redfishWalker.ps1
@@ -0,0 +1,36 @@
+﻿
+$ip = "10.72.14.119"
+
+$uri = "https://" + $ip + "/redfish/v1"
+
+$header = @{}
+$header.Add("Content-Type","application/json")
+$header.Add("Accept-Language", "en_US")
+
+# create class to handle SSL errors
+$code = @"
+public class SSLHandler
+{
+    public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
+    {
+       return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+    }
+
+}
+"@
+
+#compile the class
+if (-not ([System.Management.Automation.PSTypeName]'SSLHandler').Type)
+{
+    Add-Type -TypeDefinition $code
+}
+
+# added for JavaScript serialized object
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
+
+$respWeb = Invoke-WebRequest -Uri $uri -Method Get -Headers $header -ErrorAction Stop
+$resp = (New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer -Property @{MaxJsonLength=67108864}).DeserializeObject($respWeb)
+
+$resp
